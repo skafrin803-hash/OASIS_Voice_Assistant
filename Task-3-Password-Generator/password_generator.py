1348c856356e87bdb97b549b72a085328c035d44
skafrin803-hash <skafrin803@gmail.com>
2026-09-18 21:48:57 +0530

    Create password_generator.py

---
+++ Task-3-Password-Generator/password_generator.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_password(length):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(chars) for _ in range(length))
+    return password
+
+length = int(input("Enter password length: "))
+print("Generated Password:", generate_password(length))
